5b0d0db332659eca9eed973f17c02ffe9b7f2eb4
Anna Troian <brikodili.troyan@gmail.com>
2021-12-06 12:19:42 +0200

    Added bootstrap

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,10 +1,20 @@
-const burgerButton = document.getElementById('burger_button');
-const burgerMenu = document.getElementById('burger_menu');
-const burgerLink = document.getElementById('links')
-
-if (burgerButton) {
-    burgerButton.addEventListener("click", () => {
-        burgerMenu.classList.toggle('active')
-        document.body.classList.toggle('lock')
-    })
-} 
+// const burgerButton = document.getElementById('burger_button');
+// const burgerMenu = document.getElementById('burger_menu');
+// const burgerLink = document.getElementById('links')
+//
+// if (burgerButton) {
+//     burgerButton.addEventListener("click", () => {
+//         burgerMenu.classList.toggle('active')
+//         document.body.classList.toggle('lock')
+//     })
+// }
+//
+//
+// const resizeObserver = new ResizeObserver((entries) => {
+//     for (let entry of entries) {
+//         console.log(entry)
+//     }
+// })
+//
+// // мы можем следить за любым количеством элементов
+// resizeObserver.observe(burgerMenu);
