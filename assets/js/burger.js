04e5277255ab6b55a888071938324d011796149b
InRectoVirtus <inrectovirtus.8.32@gmail.com>
2021-12-06 11:47:25 +0200

    Fixes

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,8 +1,10 @@
 const burgerButton = document.getElementById('burger_button');
 const burgerMenu = document.getElementById('burger_menu');
+const burgerLink = document.getElementById('links')
 
 if (burgerButton) {
     burgerButton.addEventListener("click", () => {
         burgerMenu.classList.toggle('active')
+        document.body.classList.toggle('lock')
     })
-}
+} 
